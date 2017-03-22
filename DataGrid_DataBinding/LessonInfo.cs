9de9af7eea1a7f3ace64373665e6abe1484177ae
big cash <1352288123@qq.com>
2017-03-22 23:50:39 +0800

    添加项目文件。

---
+++ DataGrid_DataBinding/LessonInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGrid_DataBinding
+{
+   public  class LessonInfo
+    {
+
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+        public string Master { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
+}
---
+++ DataGrid_DataBinding/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using Prism.Commands;
+using System.Collections.Generic;
+using System.Windows.Input;
+using System;
+using System.Threading.Tasks;
+
+namespace DataGrid_DataBinding
+{
+    public class MainViewModel
+    {
+        public MainViewModel(MainWindow mainView)
+        {
+            _mainView = mainView;
+            LoadCommand = new DelegateCommand(LoadAsync);
+            SelectionChangedCommand = new DelegateCommand(SelectionChangedAsync);
+        }
+
+        private void SelectionChangedAsync()
+        {
+            object obj = _mainView.datagrid.SelectedItem;
+            Console.WriteLine(obj);
+        }
+
+        private void LoadAsync()
+        {
+            Lessons = new List<LessonInfo>();
+
+            Lessons.Add(new LessonInfo()
+                {
+                    Name = "语文",
+                    Teacher = "于丹",
+                    StartTime = "2017-03-22 22:12:21",
+                    EndTime = "2017-03-22 22:12:29",
+                    Master = "122"
+                });
+                Lessons.Add(new LessonInfo()
+                {
+                    Name = "数学",
+                    Teacher = "史冬鹏",
+                    StartTime = "2017-03-22 22:12:21",
+                    EndTime = "2017-03-22 22:12:29",
+                    Master = "122"
+                });
+                Lessons.Add(new LessonInfo()
+                {
+                    Name = "英语",
+                    Teacher = "李阳",
+                    StartTime = "2017-03-22 22:12:21",
+                    EndTime = "2017-03-22 22:12:29",
+                    Master = "122"
+                });
+
+                _mainView.datagrid.ItemsSource = Lessons;
+        }
+
+        public List<LessonInfo> Lessons { get; set; }
+        public ICommand LoadCommand { get; set; }
+        private MainWindow _mainView;
+        public ICommand SelectionChangedCommand { get; set; }
+    }
+}
